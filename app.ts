f518a68722b9475fffe6128a43cc77c140a6fd2b
Arnon Axelrod <arnonax@etoro.com>
2022-01-08 11:43:19 +0200

    Implemented /login

--- app.ts
+++ app.ts
@@ -47,6 +47,30 @@ app.post("/register", async (req, res) => {
     }
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
+    try{
+        const {email, password} = req.body;
 
+        if (!(email && password)) {
+            return res.status(400).send("All input is required");
+        }
+
+        const user = await User.findOne({email});
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign(
+                {user_id: user._id, email},
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "2h"
+                }
+            );
+
+            user.token = token;
+            return res.status(200).json(user);
+        }
+        return res.status(400).send("Invalid credentials");
+    }
+    catch(err) {
+        console.log(err);
+    }
 });
